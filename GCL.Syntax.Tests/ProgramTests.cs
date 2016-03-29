1d0c2ff1ad2ca638e1395d1b37de12c89fb7c753
Israel Cuevas <isracg@gmail.com>
2016-03-28 22:52:34 -0700

    Add test that just does the same thing as program.

---
+++ GCL.Syntax.Tests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCL.Syntax;
+using Xunit;
+using CodeParser = GCL.Syntax.CodeParser;
+
+namespace Syntax.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public static void Main()
+        {
+            var sourceCode = File.ReadAllText(@"TestData\SourceCode.txt");
+            var sourceTokens = File.ReadAllText(@"TestData\Tokens.txt");
+            var grammarCode = File.ReadAllText(@"TestData\GrammarGCL.txt");
+            var grammarTokens = File.ReadAllText(@"TestData\GrammarTokens.txt");
+            var codeParser = new CodeParser(sourceTokens, grammarTokens, grammarCode);
+            codeParser.Parse(sourceCode);
+        }
+    }
+}
