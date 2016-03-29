c09da19d6202ccd4dadd7b9aac224eee25a2b3fe
Israel Cuevas <isracg@gmail.com>
2016-03-29 00:05:05 -0700

    Add pinning test A_lexer_maintains_behavior

---
+++ GCL.Lex.Tests/LexerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace GCL.Lex.Tests
+{
+    public class LexerTests
+    {
+        [Fact]
+        public void A_lexer_maintains_behavior()
+        {
+            var grammarTokens = File.ReadAllText(@"TestData\GrammarTokens.txt");
+
+            var lexer = new Lexer(grammarTokens);
+
+            var tokenDispatchCount = 0;
+            lexer.TokenCourier += _ => tokenDispatchCount++;
+            lexer.Start(File.ReadAllText(@"TestData\SourceCode.txt"));
+
+            lexer.TokenNames.Count().Should().Be(84);
+            tokenDispatchCount.Should().Be(205);
+        }
+    }
+}
